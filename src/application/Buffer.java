9f705164535a82f7543b9547dbe970b3d64f6e1f
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2023-01-30 14:50:42 +0100

    Avancée buffer

--- src/application/Buffer.java
+++ src/application/Buffer.java
@@ -8,4 +8,33 @@ public class Buffer {
 		this.tab = new String[4];
 	}
 	
+	public void add(String string) {
+		if (!this.hasEmptySpace()) {
+			System.out.println("No empty space");
+			return;
+		}
+	}
+	
+	public boolean isEmtpy() {
+		boolean result = true;
+		for (int i = 0; i<this.tab.length; i++) {
+			if (!(this.tab[i] == null)) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	public boolean hasEmptySpace() {
+		boolean result = false;
+		for (int i = 0; i<this.tab.length; i++) {
+			if (this.tab[i] == null) {
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+	
 }
--- src/application/main1.java
+++ src/application/main1.java
@@ -1,10 +1,15 @@
 package application;
 
+import application.Buffer;
+
 public class main1 {
 
 	public static void main(String[] args) {
-		TODO();
-
+		
+		Buffer buffer = new Buffer();
+		
+		System.out.println(buffer.tab[0]);
+		
 	}
 
 }
