f2a308da25edbfb7d934a7a696cab5961a3b1c15
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-01-30 14:51:29 +0100

    changement

--- src/application/Buffer.java
+++ src/application/Buffer.java
@@ -1,40 +1,23 @@
 package application;
 
+import java.util.ArrayList;
+
 public class Buffer {
-	
-	String[] tab;
-	
-	public Buffer() {
-		this.tab = new String[4];
-	}
-	
-	public void add(String string) {
-		if (!this.hasEmptySpace()) {
-			System.out.println("No empty space");
-			return;
-		}
-	}
-	
-	public boolean isEmtpy() {
-		boolean result = true;
-		for (int i = 0; i<this.tab.length; i++) {
-			if (!(this.tab[i] == null)) {
-				result = false;
-				break;
-			}
-		}
-		return result;
-	}
-	
-	public boolean hasEmptySpace() {
-		boolean result = false;
-		for (int i = 0; i<this.tab.length; i++) {
-			if (this.tab[i] == null) {
-				result = true;
-				break;
-			}
-		}
-		return result;
-	}
-	
+  private ArrayList<String> elements;
+  private final int size = 4;
+
+  public Buffer() {
+    this.elements = new ArrayList<>(size);
+  }
+
+  public boolean hasEmptySpace() {
+    return elements.size() < size;
+  }
+
+  public String getResource(String resource) {
+    if (elements.contains(resource)) {
+      return "Element is there.";
+    }
+    return "Element not found.";
+  }
 }
